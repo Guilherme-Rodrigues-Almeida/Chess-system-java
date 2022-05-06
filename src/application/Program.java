190b45952443f1e33b073d5c792ceb53cec24c6d
Guilherme_gra <guilhermealmeida219@gmail.com>
2022-05-06 14:05:42 +0100

    Piece move count

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,10 +39,12 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
-			} catch (ChessException e) {
+			} 
+			catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
-			} catch (InputMismatchException e) {
+			} 
+			catch (InputMismatchException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}
